419468f43279f06202801dfad9d703ba9d26dfee
Ahmed Yimer <ahmed0927626158@gmail.com>
2026-08-25 20:33:46 +0330

    Update main.py

--- backendapi/app/main.py
+++ backendapi/app/main.py
@@ -21,7 +21,7 @@ app.add_middleware(SlowAPIMiddleware)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://feedback-intelligence-opal.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
