f6c869769cf233c57e120f82a23cb0e5079c9ee6
Ahmed Yimer <ahmed0927626158@gmail.com>
2026-08-25 22:11:10 +0330

    Update seed.py

--- backendapi/app/core/seed.py
+++ backendapi/app/core/seed.py
@@ -1,8 +1,7 @@
 from sqlalchemy.orm import Session
-from core.config import settings
-from crud.user import get_user_by_email, create_user
-from schemas.user import UserCreate
-
+from .config import settings
+from ..crud.user import get_user_by_email, create_user
+from ..schemas.user import UserCreate
 
 
 def seed_admin(db: Session) -> None:
